32b30a50014958e99194a13098423a0d532cdad9
frubilar1986 <rubilarfrancisco86@gmail.com>
2020-09-10 01:53:59 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio fai2426fidrive</title>
+</head>
+<body>
+    <div class="cabecera" >
+        <h2>Presentaci&oacute;n</h2>
+        <p>
+            Esta es una aplicacion para evaluar el progreso obtenido en la materia PROGRAMACION WEB DINAMICA
+        </p>
+        <p>NOMBRE: RUBILAR FRANCISCO</p>
+        <P>LEGAJO: FAI2426</P>
+    </div>
+</body>
+</html>
